4801e4016d63f36c8e9fea977f935bdbd71befe7
Ali <vynz420@gmail.com>
2025-05-11 10:13:22 +0500

    sas

--- example.py
+++ example.py
@@ -89,7 +89,7 @@ async def entrypoint(ctx: JobContext):
         # any combination of STT, LLM, TTS, or realtime API can be used
         llm=openai.LLM(model="gpt-4o-mini"),
         stt=deepgram.STT(model="nova-3", language="multi"),
-        tts=openai.TTS(voice="ash"),
+        tts=None,
         # use LiveKit's turn detection model
         turn_detection=None,
     )
