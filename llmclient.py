ca061154ae0ad5de0f771bfba91ccf70f5bdf1e8
Ali <vynz420@gmail.com>
2025-05-11 17:12:25 +0500

    asa

--- llmclient.py
+++ llmclient.py
@@ -50,7 +50,7 @@ from models import (
     TogetherChatModels,
     XAIChatModels,
 )
-from .utils import AsyncAzureADTokenProvider, to_chat_ctx, to_fnc_ctx
+from utils import AsyncAzureADTokenProvider, to_chat_ctx, to_fnc_ctx
 
 
 @dataclass
